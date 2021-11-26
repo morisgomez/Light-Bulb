a1d902c8166b6030b76ceff0f3d1863a91b6b8e3
Moris Gomez <83724852+morisgomez@users.noreply.github.com>
2021-11-25 20:24:58 -0800

    Add files via upload

---
+++ light-bulb/javascript/bulb.js
@@ -0,0 +1,9 @@
+function bulbOff () {
+    document.getElementById("image").src = "https://www.w3schools.com/js/pic_bulboff.gif";
+    }
+
+function bulbOn () {
+    document.getElementById("image").src = "https://www.w3schools.com/js/pic_bulbon.gif";
+    }
+    
+    
